fc8b9739ccf409f0255cdadae56129b4ecbed67a
Azaemorales2329 <bitsplash1314@gmail.com>
2019-10-17 19:35:44 -0500

    commit

--- app/src/main/java/com/example/proyectoemergentes/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/proyectoemergentes/ui/dashboard/DashboardFragment.java
@@ -2,9 +2,13 @@ package com.example.proyectoemergentes.ui.dashboard;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -31,5 +35,24 @@ public class DashboardFragment extends Fragment {
             }
         });
         return root;
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.toolbar_menu,menu);
+        menu.findItem(R.id.action_qr_code).setVisible(true);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.action_qr_code){
+            Toast.makeText(getContext(),"CÓDIGO QR",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
